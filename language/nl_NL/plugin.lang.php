c35a58a712bf76436f7def3e7731c8f3fa354a59
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-22 22:08:50 +0000

    [guestbook] Update nl_NL, thanks to : EdwinKort
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@15953 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s zegt op %s:';
+$lang['* : mandatory fields'] = '* : verplichte veld';
+$lang['Activate rating'] = 'Activeren waardering';
+$lang['GuestBook'] = 'Gastenboek';
+$lang['Please enter your username'] = 'Voer uw gebruikersnaam in';
+$lang['Sign the guestbook'] = 'Teken het gastenboek';
+$lang['There are %d messages'] = 'Er zijn %d berichtten';
+$lang['Website'] = 'Website';
+$lang['invalid website address'] = 'foute website adres';
+$lang['not publicly visible'] = 'niet publieke zichtbaar';
+?>
