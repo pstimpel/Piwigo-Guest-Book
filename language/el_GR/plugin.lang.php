cf649d8dae5b98365169db16047ec351f2008fe9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-23 09:46:10 +0000

    [guestbook] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@15963 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s λέει στο %s :';
+$lang['* : mandatory fields'] = '*: Υποχρεωτικά πεδία';
+$lang['Activate rating'] = 'Ενεργοποίηση βαθμολογίας';
+$lang['GuestBook'] = 'Βιβλίο Επισκεπτών';
+$lang['Please enter your username'] = 'Παρακαλώ εισάγετε το username σας';
+$lang['Sign the guestbook'] = 'Υπογράψτε το βιβλίο επισκεπτών';
+$lang['There are %d messages'] = 'Υπάρχουν %d μηνύματα';
+$lang['Website'] = 'Δικτυακός Τόπος';
+$lang['invalid website address'] = 'μη έγκυρη διεύθυνση δικτυακού τόπου';
+$lang['not publicly visible'] = 'δεν είναι ορατή δημοσίως';
+?>
