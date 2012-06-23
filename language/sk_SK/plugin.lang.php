087e1c492c6c766f8a511da1340f1b1a7fffb65a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-23 17:36:15 +0000

    [guestbook] Update sk_SK, thanks to : xpace
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@15971 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s povedal/a d&#328;a %s ';
+$lang['* : mandatory fields'] = '*:povinn&eacute; polia';
+$lang['Activate rating'] = 'Aktivova&#357; hodnotenie';
+$lang['GuestBook'] = 'Kniha n&aacute;v&#353;tev';
+$lang['Please enter your username'] = 'Pros&iacute;m, zadajte svoje u&#382;&iacute;vate&#314;sk&eacute; meno';
+$lang['Sign the guestbook'] = 'Prihl&aacute;si&#357; sa do knihy n&aacute;v&#353;tev';
+$lang['There are %d messages'] = 'Je zap&iacute;san&yacute;ch %d odkazov';
+$lang['Website'] = 'Webov&aacute; str&aacute;nka';
+$lang['invalid website address'] = 'neplatn&aacute; webov&aacute; adresa';
+$lang['not publicly visible'] = 'verejne nezvidite&#314;nen&eacute;';
+?>
