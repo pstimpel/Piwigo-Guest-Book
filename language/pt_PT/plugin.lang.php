23c96e430b70b43778b1d0a4a7ff21d34ca85f15
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-23 09:46:19 +0000

    [guestbook] Update pt_PT, thanks to : sda
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@15964 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s disse em %s:';
+$lang['* : mandatory fields'] = '*: campos obrigatórios';
+$lang['Activate rating'] = 'Ativar classificação';
+$lang['GuestBook'] = 'Livro de visitantes';
+$lang['Please enter your username'] = 'Por favor digite o seu username';
+$lang['Sign the guestbook'] = 'Assine o livro de visitantes';
+$lang['There are %d messages'] = 'Há %d mensagens';
+$lang['Website'] = 'Sítio web';
+$lang['invalid website address'] = 'Morada do sítio web inválida';
+$lang['not publicly visible'] = 'Não visível publicamente';
+?>
