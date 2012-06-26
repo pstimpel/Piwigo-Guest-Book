40219276113d69bd90f1bb88fa8c9e8559556c4e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-26 17:10:31 +0000

    [guestbook] Update uk_UA, thanks to : rukhov
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@16059 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s сказав про %s:';
+$lang['* : mandatory fields'] = '*:обов\'язкові поля';
+$lang['Activate rating'] = 'Активізувати рейтинг';
+$lang['GuestBook'] = 'Гостьова книга';
+$lang['Please enter your username'] = 'Будь ласка, введіть ваше ім\'я користувача';
+$lang['Sign the guestbook'] = 'Залишити підпис в гостьовій книзі';
+$lang['There are %d messages'] = '%d повідомлень';
+$lang['Website'] = 'Веб-сайт';
+$lang['invalid website address'] = 'недійсна адреса веб-сайту';
+$lang['not publicly visible'] = 'невидимий для загалу';
+?>
