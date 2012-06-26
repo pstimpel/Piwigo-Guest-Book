1071e4778e912b698c5dcd10543946490d25a31a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-26 17:10:16 +0000

    [guestbook] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@16058 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s üzerine diyor %s:';
+$lang['* : mandatory fields'] = '*: zorunlu alanlar';
+$lang['Activate rating'] = 'Değerlendirmeyi etkinleştir';
+$lang['GuestBook'] = 'Ziyaretçi Defteri';
+$lang['Please enter your username'] = 'Lütfen kullanıcı adınızı giriniz';
+$lang['Sign the guestbook'] = 'Ziyaretçi defterini imzala';
+$lang['There are %d messages'] = '%d adet mesaj var';
+$lang['Website'] = 'Web sitesi';
+$lang['invalid website address'] = 'geçersiz web sitesi adresi';
+$lang['not publicly visible'] = 'herkese görünür değil';
+?>
