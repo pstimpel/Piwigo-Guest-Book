ff531194257a188c8f9b27e565406d1f48435270
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-29 04:00:48 +0000

    [guestbook] Update zh_CN, thanks to : quqibing
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@16153 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s 评论 %s :';
+$lang['* : mandatory fields'] = '* : 必填字段';
+$lang['Activate rating'] = '启用评分';
+$lang['GuestBook'] = '在线评论';
+$lang['Please enter your username'] = '请输入您的用户名';
+$lang['Sign the guestbook'] = '我要留言';
+$lang['There are %d messages'] = '有%d个留言';
+$lang['Website'] = '网站';
+$lang['invalid website address'] = '无效网站地址';
+$lang['not publicly visible'] = '不公开';
+?>
