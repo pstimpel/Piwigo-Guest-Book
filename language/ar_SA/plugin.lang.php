e8392a0f325efd310f573f964e9eda045c064ca1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-05 15:55:36 +0000

    [guestbook] Update ar_SA, thanks to : mohammed
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@16361 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ar_SA/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s يقول  %s :';
+$lang['* : mandatory fields'] = '* : خانات إلزاميه';
+$lang['Activate rating'] = 'تفعيل التقييم';
+$lang['GuestBook'] = 'سجل الزوار';
+$lang['Please enter your username'] = 'فضلا ً أكتب أسمك الكريم';
+$lang['Sign the guestbook'] = 'أترك بصمتك الخاصه في سجل الزوار';
+$lang['There are %d messages'] = 'هناك %d  كلمه ';
+$lang['Website'] = 'الموقع';
+$lang['invalid website address'] = 'عنوان الموقع غير صحيح ';
+$lang['not publicly visible'] = 'لن يكون ظاهر';
+?>
