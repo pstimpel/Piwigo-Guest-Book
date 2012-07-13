e4a66f90337cd3aad1bd0ff456d2fb291039adfa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-13 21:50:10 +0000

    [guestbook] Update hu_HU, thanks to : asicu.com
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@16702 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = 'ezt mondja %s ekkor: %s :';
+$lang['* : mandatory fields'] = '* : kötelező mezők';
+$lang['Activate rating'] = 'Értékelés bekapcsolása';
+$lang['GuestBook'] = 'Vendégkönyv';
+$lang['Please enter your username'] = 'Add meg a felhasználó nevedet';
+$lang['Sign the guestbook'] = 'Írd alá a vendégkönyvet';
+$lang['There are %d messages'] = 'Üzenetek száma: %d';
+$lang['Website'] = 'Honlap';
+$lang['invalid website address'] = 'Érvénytelen honlap cím';
+$lang['not publicly visible'] = 'nyilvánosan nem látható (nem publikus)';
+?>
