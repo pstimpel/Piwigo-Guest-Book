e7d893da6cef6bed094b6548a0e95af0a2234a33
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-15 13:56:33 +0000

    [guestbook] Update ro_RO, thanks to : decadnce
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@16749 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s a zis in %s :';
+$lang['* : mandatory fields'] = '* : campuri obligatorii';
+$lang['Activate rating'] = 'Activeaza evaluarea';
+$lang['GuestBook'] = 'Carte de oaspeti';
+$lang['Please enter your username'] = 'Introduceti numele de utilizator';
+$lang['Sign the guestbook'] = 'Semneaza cartea de oaspeti';
+$lang['There are %d messages'] = 'Acolo sunt %d mesaje';
+$lang['Website'] = 'Site web';
+$lang['invalid website address'] = 'Adresa site web invalida';
+$lang['not publicly visible'] = 'nevizibil publicului';
+?>
