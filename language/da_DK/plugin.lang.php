587395e48e78dae18f15e20f5ac28461aafa8dad
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-21 07:28:49 +0000

    [guestbook] Update da_DK, thanks to : Egorip
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@16880 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s Siger på %s :';
+$lang['* : mandatory fields'] = '*: Obligatoriske felter';
+$lang['Activate rating'] = 'Aktiver vurdering';
+$lang['GuestBook'] = 'Gæstebog';
+$lang['Please enter your username'] = 'Indtast dit brugernavn';
+$lang['Sign the guestbook'] = 'Skriv i gæstebogen';
+$lang['There are %d messages'] = 'Der er %d beskeder';
+$lang['Website'] = 'websted';
+$lang['invalid website address'] = 'ugyldig hjemmeside adresse';
+$lang['not publicly visible'] = 'ikke offentligt tilgængelige';
+?>
