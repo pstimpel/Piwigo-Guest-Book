4f989bcec6ac2368256738c05bb59e8a6d4a604b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-01 15:41:32 +0000

    [guestbook] Update bg_BG, thanks to :
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@17283 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['not publicly visible'] = 'не е видимо публично';
+$lang['Sign the guestbook'] = 'Вписване в книга на гостите';
+$lang['Please enter your username'] = 'Моля въведете вашето име';
+$lang['GuestBook'] = 'Книга за гости';
+$lang['Activate rating'] = 'Активиране на рейтинг';
+$lang['* : mandatory fields'] = '*: задължителни полета';
+$lang['%s says on %s :'] = '%s казва на %s :';
+$lang['invalid website address'] = 'невалиден уебсайт адрес';
+$lang['Website'] = 'Уебсайт';
+$lang['There are %d messages'] = 'Имате %d съобщения';
+?>
