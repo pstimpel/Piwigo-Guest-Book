ad3947688b0dc4cc8578b6ba4809eec3250533da
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-04 21:41:57 +0000

    [guestbook] Update fi_FI, thanks to : Juhi24
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@19806 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Activate rating'] = 'Ota arvosanat käyttöön';
+$lang['There are %d messages'] = '%d viestiä';
+$lang['not publicly visible'] = 'ei näkyvillä julkisesti';
+$lang['invalid website address'] = 'virheellinen internet-osoite';
+$lang['Website'] = 'Sivusto';
+$lang['Sign the guestbook'] = 'Kirjoita vieraskirjaan';
+$lang['Please enter your username'] = 'Ole hyvä ja anna käyttäjänimesi';
+$lang['GuestBook'] = 'Vieraskirja';
+$lang['* : mandatory fields'] = '* : pakolliset kentät';
+$lang['%s says on %s :'] = '%s sanoi %s :';
+?>
