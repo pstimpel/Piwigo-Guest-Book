11641eff10427abc5fee21a0dd96b0994d62975a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-04 07:31:01 +0000

    [guestbook] Add sv_SE, thanks to : AndersS
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@21171 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s säger på %s :';
+$lang['* : mandatory fields'] = '* :obligatoriska fält';
+$lang['Activate rating'] = 'Aktivera betygssättning';
+$lang['GuestBook'] = 'Gästbok';
+$lang['Please enter a message'] = 'Mata in meddelande';
+$lang['Please enter your e-mail'] = 'Mata in din epostadress';
+$lang['Please enter your username'] = 'Mata in ditt användarnamn';
+$lang['Sign the guestbook'] = 'Signera gästboken';
+$lang['There are %d messages'] = 'Det till %d meddelanden';
+$lang['Website'] = 'Webbsida';
+$lang['invalid website address'] = 'Ogiltig webadress';
+$lang['not publicly visible'] = 'Ej publikt synlig';
+?>
