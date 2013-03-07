25e841b32f8ceb882731a9a0d79e2217b403755e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-07 15:04:21 +0000

    [guestbook] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@21318 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s 說 %s :';
+$lang['* : mandatory fields'] = '* : 必填欄位';
+$lang['Activate rating'] = '啟動評級';
+$lang['GuestBook'] = '訪客留言簿';
+$lang['Please enter a message'] = '請輸入留言';
+$lang['Please enter your e-mail'] = '請輸入您的電子郵件';
+$lang['Please enter your username'] = '請輸入您的使用者名稱';
+$lang['Sign the guestbook'] = '我要留言';
+$lang['There are %d messages'] = '有 %d 封郵件';
+$lang['Website'] = '網站';
+$lang['invalid website address'] = '網頁無效';
+$lang['not publicly visible'] = '不公開顯示的';
+?>
