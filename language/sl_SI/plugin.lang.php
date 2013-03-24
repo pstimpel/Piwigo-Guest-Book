7590ae75644dc2c3d22c54132a43122a4f71e4a4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-24 18:27:18 +0000

    [guestbook] Add sl_SI, thanks to : MarkoAm
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@21820 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s je za %s napisal/a :';
+$lang['* : mandatory fields'] = '* : obvezna polja';
+$lang['Activate rating'] = 'Vključi ocenjevanje';
+$lang['GuestBook'] = 'Knjiga gostov';
+$lang['Please enter a message'] = 'Prosimo napišite sporočilo';
+$lang['Please enter your e-mail'] = 'Prosimo vpišite svoj e-mail';
+$lang['Please enter your username'] = 'Prosimo vpišite svoje uporabniško ime';
+$lang['Sign the guestbook'] = 'Vpišite se v knjigo gostov';
+$lang['There are %d messages'] = 'Tu je %d sporočil';
+$lang['Website'] = 'Spletna stran';
+$lang['invalid website address'] = 'napačen naslov spletne stani';
+$lang['not publicly visible'] = 'ni javno vidno';
+?>
