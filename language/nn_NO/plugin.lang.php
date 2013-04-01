9a050f7e311cc6b1872502118b68ad32567e711a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-01 16:43:13 +0000

    [guestbook] Add nn_NO, thanks to : kiho
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@21939 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nn_NO/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s skriv på %s';
+$lang['* : mandatory fields'] = '*: påkrevde felt';
+$lang['Activate rating'] = 'Aktiver rangering';
+$lang['GuestBook'] = 'Gjestebok';
+$lang['Please enter a message'] = 'Skriv inn ei melding';
+$lang['Please enter your e-mail'] = 'Skriv inn di e-postadresse';
+$lang['Please enter your username'] = 'Skriv inn brukarnamnet ditt';
+$lang['Sign the guestbook'] = 'Skriv i gjesteboka';
+$lang['There are %d messages'] = 'Det finst %d meldingar';
+$lang['Website'] = 'Nettside';
+$lang['invalid website address'] = 'ugyldig nettsideadresse';
+$lang['not publicly visible'] = 'ikkje synleg for alle';
+?>
