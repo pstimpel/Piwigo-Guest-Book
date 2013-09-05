657a9dd4629a86ac5cc8ae5059d2ec1552b29d1f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-05 04:08:18 +0000

    [guestbook] Update vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@24345 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s nói %s :';
+$lang['* : mandatory fields'] = '* : Các trường bắt buộc';
+$lang['Activate rating'] = 'Kích hoạt đánh giá';
+$lang['GuestBook'] = 'Guestbook';
+$lang['Please enter a message'] = 'Hãy nhập một tin nhắn';
+$lang['Please enter your e-mail'] = 'Hãy nhập địa chỉ e-mail';
+$lang['Please enter your username'] = 'Hãy nhập tên người dùng';
+$lang['Sign the guestbook'] = 'Ký vào guestbook';
+$lang['There are %d messages'] = 'Có %d tin nhắn';
+$lang['Website'] = 'Website';
+$lang['invalid website address'] = 'địa chỉ website không hợp lệ';
+$lang['not publicly visible'] = 'không hiển thị rộng rãi';
+?>
