694c081029428babacdcbfebdbf837bf14f4bbee
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-28 09:35:52 +0000

    [guestbook] Update lt_LT, thanks to : vilisas
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@26274 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lt_LT/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Sign the guestbook'] = 'Pasirašyti svečių knygoje';
+$lang['%s says on %s :'] = '%s pasakė %s:';
+$lang['* : mandatory fields'] = '*: privalomi laukeliai';
+$lang['Activate rating'] = 'Aktyvuoti reitingą';
+$lang['GuestBook'] = 'Svečių knyga';
+$lang['Guests (not registered) permissions :'] = 'Neregistruotų svečių leidimai:';
+$lang['Please enter a message'] = 'Parašykite žinutę';
+$lang['Please enter your e-mail'] = 'Įveskite savo e-mail';
+$lang['Please enter your username'] = 'Įveskite savo username';
+$lang['There are %d messages'] = 'Yra žinučių: %d';
+$lang['Website'] = 'Puslapis';
+$lang['add comment'] = 'pridėti komentarą';
+$lang['invalid website address'] = 'neteisingas puslapio adresas';
+$lang['not publicly visible'] = 'viešai nematomas';
+$lang['view comments'] = 'skaityti komentarus';
+?>
