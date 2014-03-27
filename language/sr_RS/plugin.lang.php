a7406edd6889b6b44c8e8d22fef67fe716707f4f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-03-27 06:21:50 +0000

    [guestbook] Update sr_RS, thanks to : gpopac
    git-svn-id: http://piwigo.org/svn/extensions/GuestBook@27985 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s је рекао на %s:';
+$lang['* : mandatory fields'] = '*: обавезна поља';
+$lang['Activate rating'] = 'Омогући оцењивање';
+$lang['GuestBook'] = 'Књига гостију';
+$lang['Guests (not registered) permissions :'] = 'Дозволе за госта (непријављеног):';
+$lang['Please enter a message'] = 'Унесите поруку';
+$lang['Please enter your e-mail'] = 'Унесите вашу е-адресу';
+$lang['Please enter your username'] = 'Унесите ваше корисничко име';
+$lang['Rate'] = 'Оцени';
+$lang['Send'] = 'Пошаљи';
+$lang['Sign the guestbook'] = 'Упиши се у књигу гостију';
+$lang['There are %d messages'] = 'Има %d порука';
+$lang['Website'] = 'Веб страница';
+$lang['add comment'] = 'додај коментар';
+$lang['invalid website address'] = 'неисправна адреса веб странице';
+$lang['not publicly visible'] = 'није јавно видљиво';
+$lang['view comments'] = 'погледај коментаре';
