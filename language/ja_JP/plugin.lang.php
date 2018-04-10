5ad4cba780def0b0c44d93d8c04da11ee54dd7b7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-10 14:16:58 +0200

    Add ja_JP, thanks to Trumpet

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['not publicly visible'] = '公には見えない';
+$lang['* : mandatory fields'] = '*: 必須項目です';
+$lang['Activate rating'] = 'レーティングを有効化する';
+$lang['Add link in menu'] = 'メニューにリンクを追加する';
+$lang['GuestBook'] = 'ゲストブック';
+$lang['Guests (not registered) permissions :'] = '（登録済みでない）ゲストの権限：';
+$lang['Please enter a message'] = 'メッセージを入力して下さい';
+$lang['Please enter your e-mail'] = 'あなたのメールアドレスを入力して下さい';
+$lang['Please enter your username'] = 'あなたのユーザ名を入力して下さい';
+$lang['Rate'] = 'レート';
+$lang['Send'] = '送信する';
+$lang['Sign the guestbook'] = 'ゲストブックにサインする';
+$lang['There are %d messages'] = '%d個のメッセージがあります';
+$lang['Website'] = 'ウェブサイト';
+$lang['add comment'] = 'コメントを追加する';
+$lang['invalid website address'] = '無効なウェブサイトアドレス';
+$lang['view comments'] = 'コメントを見る';
